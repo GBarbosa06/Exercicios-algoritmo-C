21c3d5db581ef2a6aaca4f0390adca82823061f3
GBarbosa <103224622+GBarbosa06@users.noreply.github.com>
2024-09-21 09:38:39 -0300

    ex001

---
+++ Macros-e-Funcoes/ex001.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int verificaN(float n);
+
+int main()
+{
+    float n;
+
+    printf("Digite um numero: ");
+    scanf("%f", n);
+
+    verificaN(n);
+}
+
+int verificaN(float n)
+{
+    if (n > 0)
+    {
+        return 1;
+    }
+    else
+    {
+        if (n < 0)
+        {
+            return 0;
+        }
+        else
+        {
+            printf("Digite um numero diferente de zero");
+            verificaN(n);
+        }
+        
+    }
+    
+}

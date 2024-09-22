e1937b850cbf84cae5ce39bb3c0ab3640386ffb5
GBarbosa <andradegui2006@gmail.com>
2024-09-22 11:57:39 -0300

    ex06 completo
    O unico erro era o espaço antes do %c, no scanf

--- Macros-e-Funcoes/ex006.c
+++ Macros-e-Funcoes/ex006.c
@@ -12,15 +12,17 @@ int main()
     scanf("%f %f %f", &n1, &n2, &n3);
 
     printf("Escolha: \n A. media aritmetica \n P. media ponderada\n");
-    scanf("%c", &tipo);
+    scanf(" %c", &tipo);
 
     switch(tipo)
     {
         case 'A':
+        case 'a':
             printf("Media aritmetica = %.2f", mediaAri(n1, n2, n3));
             break;
             
         case 'P':
+        case 'p':
             printf("Media ponderada = %.2f", mediaPond(n1, n2, n3));
             break;
         

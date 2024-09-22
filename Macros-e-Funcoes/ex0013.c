38f3b2e12769bf9f7ddfde1ebce9a940269a96cb
GBarbosa <andradegui2006@gmail.com>
2024-09-22 15:17:14 -0300

    correção ex0013

--- Macros-e-Funcoes/ex0013.c
+++ Macros-e-Funcoes/ex0013.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+#define multiplo(a, b) ((a%b==0) ? 1:0)
+
 int main()
 {
+    int a, b;
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &a, &b);
+
+    if (multiplo(a,b) == 1) //apenas para dar um resultado visivel
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
     
 }

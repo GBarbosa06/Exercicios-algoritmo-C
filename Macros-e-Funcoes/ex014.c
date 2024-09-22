b148d53bab868b93849e8208705bcf26aa1dad7f
GBarbosa <andradegui2006@gmail.com>
2024-09-22 15:35:31 -0300

    ex0015

--- Macros-e-Funcoes/ex014.c
+++ Macros-e-Funcoes/ex014.c
@@ -2,7 +2,8 @@
 
 #define IS_VOGAL(c) ((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u')||(c=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U'))
 
-int main() {
+int main() 
+{
     char letra;
 
     printf("Digite uma letra: ");
---
+++ Macros-e-Funcoes/ex015.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define ABS(n) (n < 0 ? -n:n)
+
+int main() 
+{
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    printf("O valor absoluto de %d e %d", n, ABS(n));
+
+    return 0;
+}

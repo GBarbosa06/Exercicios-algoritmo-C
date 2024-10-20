1e621fe13967cb92cf8e1d9579a0a228e7b490d7
GBarbosa <andradegui2006@gmail.com>
2024-10-19 23:50:02 -0300

    ex002

---
+++ Recursividade e Ponteiros/ex002.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void regr(int n);
+void bin(int n);
+
+
+int main()
+{
+    int n;
+
+    printf("Digite um numero positivo: ");
+    scanf("%d", &n);
+
+    printf("Contagem regressiva: \n");
+    regr(n);
+    printf("Seu numero em binario eh: ");
+    bin(n);
+    
+}
+
+void regr(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        printf("%d\n", n);
+        return(regr(n-1));
+    }
+    
+}
+
+void bin(int n) 
+{
+    if (n > 0) {
+        bin(n / 2);
+        printf("%d", n % 2);
+    }
+}

222c8a7e6c5d9ceb617a1770dfaacace16ac256b
GBarbosa <andradegui2006@gmail.com>
2024-10-22 19:55:31 -0300

    correção de variáveis não usadas

--- Recursividade e Ponteiros/ex013.c
+++ Recursividade e Ponteiros/ex013.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void incrementarDecrementar(int *valor, int *incrementado, int *decrementado);
+void incrementarDecrementar(int *incrementado, int *decrementado);
 
 int main()
 {
     int valor = 5;
     int incrementado = valor, decrementado = valor;
-    int *ptr = &valor, *ptrIncremento = &incrementado, *ptrDecremento = &decrementado;
+    int *ptrIncremento = &incrementado, *ptrDecremento = &decrementado;
 
-    incrementarDecrementar(ptr, ptrIncremento, ptrDecremento);
+    incrementarDecrementar(ptrIncremento, ptrDecremento);
     printf("%d %d %d", valor, incrementado, decrementado);
 }
 
-void incrementarDecrementar(int *valor, int *incrementado, int *decrementado)
+void incrementarDecrementar(int *incrementado, int *decrementado)
 {
     (*incrementado)++;
     *decrementado = *decrementado - 2;

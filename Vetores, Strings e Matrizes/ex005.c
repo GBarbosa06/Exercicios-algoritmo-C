4487d0029819c3f779ea4453b0f45a4450c462b7
GBarbosa <andradegui2006@gmail.com>
2024-10-26 16:01:03 -0300

    ex005

--- Vetores, Strings e Matrizes/ex005.c
+++ Vetores, Strings e Matrizes/ex005.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-void carrega(int v[], int n, );
-int main()
-{
+void carrega(int *v, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Digite o valor %d: ", i+1);
+        scanf("%d", v);
+        v++;
+    }
+}
 
+int main() {
+    int n;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n);
+
+    int vetor[n];
+    carrega(vetor, n);
+    for (int i = 0; i < n; i++) {
+        printf("Valor %d: %d\n", i+1, vetor[i]);
+    }
 }

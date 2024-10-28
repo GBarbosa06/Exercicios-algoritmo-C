f3b6798b537cc8c93599288765bc89b98bf76400
GBarbosa <andradegui2006@gmail.com>
2024-10-27 21:56:02 -0300

    Modificação da questão 7

--- Vetores, Strings e Matrizes/ex007.c
+++ Vetores, Strings e Matrizes/ex007.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-void strcpyT(char s[], char t[])
-{
-    int i = 0;
-    while(t[i] != '\0')
+void strcpyT(char s[], char t[], int i)
+{   
+    int j = 0; 
+    while(t[j] != '\0')
     {
-        s[i] = t[i];
-
-        i++;
+        s[i] = t[j];
+        i++; //i é para pegar toda o texto já existente na string
+        j++; //j é para zerar a contagem da nova string
     }
+    s[i] = '\n';
     
     
 }
 
 int main()
 {
-    char string[6];
-    strcpyT(string, "teste");
-    printf("Nova string: '%s'", string);
-
+    char string[] = "Texto: ";
+    int i = (sizeof(string) - 1);
+    strcpyT(string, "Teste", i);
+    printf("%s", string);
 }
 

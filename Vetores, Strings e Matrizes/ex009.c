60c3e99f2f37bfba03e37ad425b74465f53693ed
GBarbosa <andradegui2006@gmail.com>
2024-10-28 23:43:27 -0300

    ex009

--- Vetores, Strings e Matrizes/ex009.c
+++ Vetores, Strings e Matrizes/ex009.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-void strcpyT(char s[], char t[], int i)
+void strcaT(char s[], char t[], int i)
 {   
     int j = 0; 
     while(t[j] != '\0')
     {
         s[i] = t[j];
-        i++; //i é para pegar toda o texto já existente na string
-        j++; //j é para zerar a contagem da nova string
+        i++;
+        j++;
     }
     s[i] = '\0';
-    
     
 }
 
@@ -18,7 +17,6 @@ int main()
 {
     char string[] = "Texto: ";
     int i = (sizeof(string) - 1);
-    strcpyT(string, "Teste", i);
+    strcaT(string, "Teste", i);
     printf("%s", string);
 }
-

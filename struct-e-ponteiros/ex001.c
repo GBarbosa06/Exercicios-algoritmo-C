b03756fa613ef3e43a88c3d912b8e13eb6afd6a7
GBarbosa <andradegui2006@gmail.com>
2024-11-03 13:13:11 -0300

    ex01 - uso de struct anonima

--- struct-e-ponteiros/ex001.c
+++ struct-e-ponteiros/ex001.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     struct
     {
-        char titulo[] = "Harry Potter e a Ordem da Fênix";
-
+        char titulo[50];
+        char autor[50];
+        char editora[50];
+        int anoPublicacao;
     }livro;
+    strcpy(livro.titulo, "Harry Potter e a Ordem da Fênix");
+    strcpy(livro.autor, "J. K. Rowling");
+    strcpy(livro.editora, "Rocco");
+    livro.anoPublicacao = 2003;
+
+    printf(" %s \n %s \n %s \n %d", livro.titulo, livro.autor, livro.editora, livro.anoPublicacao);
 }

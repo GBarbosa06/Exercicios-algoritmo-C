62ce99eef87a790f6ba3f86af3e87ec7bfd8934d
GBarbosa <andradegui2006@gmail.com>
2024-11-03 13:27:28 -0300

    Create ex002.c

---
+++ struct-e-ponteiros/ex002.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct
+{
+    float real;
+    float imaginario;
+}complexo;
+
+somaComplexos(complexo n1, complexo n2)
+{
+    complexo result;
+    result.real
+}
+
+int main()
+{
+
+}

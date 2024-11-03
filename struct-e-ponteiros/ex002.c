72fb75f3ac3799e5a56e5e10621b862b93a458d8
GBarbosa <andradegui2006@gmail.com>
2024-11-03 13:38:03 -0300

    ex002 soma de numeros complexos

--- struct-e-ponteiros/ex002.c
+++ struct-e-ponteiros/ex002.c
@@ -6,13 +6,23 @@ typedef struct
     float imaginario;
 }complexo;
 
-somaComplexos(complexo n1, complexo n2)
+complexo somaComplexos(complexo n1, complexo n2)
 {
     complexo result;
-    result.real
+    result.real = n1.real + n2.real;
+    result.imaginario = n1.imaginario + n2.imaginario;
+    return result;
 }
 
 int main()
 {
+    complexo n1, n2;
 
+    n1.real = 5;
+    n1.imaginario = 3;
+    n2.real = 9;
+    n2.imaginario = 1;
+
+    complexo resultado = somaComplexos(n1, n2);
+    printf("A soma (%.2f + %.2fi) + (%.2f + %.2fi) = %.2f + %.2fi", n1.real, n1.imaginario, n2.real, n2.imaginario ,resultado.real, resultado.imaginario);
 }
